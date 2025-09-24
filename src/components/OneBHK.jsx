40c65b1c2a433a4b9404cef9ca058435b4dab56f
Crimson2424 <priyankmiles@gmail.com>
2025-09-24 15:29:36 +0530

    Updated 1BHk

--- src/components/OneBHK.jsx
+++ src/components/OneBHK.jsx
@@ -1,48 +1,93 @@
 import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, useTexture } from '@react-three/drei'
+import { useControls } from 'leva';
 
 export function OneBHK(props) {
-  const { nodes, materials } = useGLTF('models/Base_Floor_1BHK-v1.glb')
+  const { nodes } = useGLTF('models/1BHK_Baked-v1.glb')
+
+  const {colorOneBHk} = useControls({
+    colorOneBHk:{
+      value: '#B8AFA2'
+    }
+  })
+
+    //Plane texture
+    const planTexture = useTexture("textures/1BHK_Bakes/Plane.png");
+    planTexture.flipY = false;
+
+    //Gold Texture
+    const goldTexture = useTexture('textures/1BHK_Bakes/Gold.png')
+    goldTexture.flipY = false;
+
+    //Livingroom Texture
+    const livingroomTexture = useTexture('textures/1BHK_Bakes/Livingroom.png')
+    livingroomTexture.flipY = false;
+
+    //Livingroom Texture
+    const bedSofaTexture = useTexture('textures/1BHK_Bakes/Bed_Sofa.png')
+    bedSofaTexture.flipY = false;
+
+    //Livingroom Texture
+    const bedroomTexture = useTexture('textures/1BHK_Bakes/Bedroom.png')
+    bedroomTexture.flipY = false;
+
+    //base Texture
+    const baseTexture = useTexture('textures/1BHK_Bakes/Base.png')
+    baseTexture.flipY = false;
+
+    //Floor Texture
+    const floorTexture = useTexture('textures/1BHK_Bakes/Floor.png')
+    floorTexture.flipY = false;
+
+    //Glass Texture
+    const glassTexture = useTexture('textures/1BHK_Bakes/Glasses.png')
+    glassTexture.flipY = false;
+    
+
   return (
     <group {...props} dispose={null}>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Walls_1.geometry}
-        material={materials.Main_walls}
-      />
+        geometry={nodes.Gold.geometry}
+      >
+        <meshBasicMaterial map={goldTexture}/>
+      </mesh>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Walls_2.geometry}
-        material={materials.Material___2147483638}
-      />
+        geometry={nodes.Livingroom.geometry}
+      >
+        <meshBasicMaterial map={livingroomTexture}/>
+      </mesh>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Floor_1.geometry}
-        material={materials.Material__2147482900}
-      />
+        geometry={nodes.Bed_Sofa.geometry}
+      >
+        <meshBasicMaterial map={bedSofaTexture}/>
+      </mesh>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Floor_2.geometry}
-        material={materials.Material__2147482921}
-      />
+        geometry={nodes.Bedroom.geometry}
+        >
+          <meshBasicMaterial map={bedroomTexture}/>
+        </mesh>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Windows_1.geometry}
-        material={materials.Glass_Clear}
-      />
+        geometry={nodes.Base.geometry}
+      >
+        <meshBasicMaterial map={baseTexture}/>
+      </mesh>
       <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Windows_2.geometry}
-        material={materials.Material__2147482897}
-      />
+        geometry={nodes.Plane.geometry}
+      >
+        <meshBasicMaterial color={'#d0c6b7'} aoMap={planTexture}/>
+      </mesh>
+      <mesh
+        geometry={nodes.Floor.geometry}
+      >
+        <meshBasicMaterial map={floorTexture}/>
+      </mesh>
+      <mesh
+        geometry={nodes.Glasses.geometry}
+      >
+        <meshBasicMaterial map={glassTexture}/>
+      </mesh>
     </group>
   )
 }
 
-useGLTF.preload('models/Base_Floor_1BHK-v1.glb')
+useGLTF.preload('models/1BHK_Baked-v1.glb')
